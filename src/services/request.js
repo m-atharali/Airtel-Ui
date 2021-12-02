1fd4fda78af42cf6421f10107674daf1e4c82d0f
saqibshahid <saqib.shahid44@gmail.com>
2021-12-02 14:44:15 +0500

    set

--- src/services/request.js
+++ src/services/request.js
@@ -1,8 +1,8 @@
 import Axios from "axios";
 
 // export var diaspora = "https://nimc-service.herokuapp.com";
-export var baseUrl = "http://localhost:5000/api/";
-
+// export var baseUrl = "http://localhost:5000/api/";
+export var baseUrl = "https://facial-airtel.herokuapp.com/api/";
 export var postData = (route, data) => {
   return new Promise((resolve, reject) => {
     Axios.post(baseUrl + route, data)
