956cc7300ad8233e442b4b97f503ebd01e11a1be
saqib shahid <sshahid.teo.aux@gotoko.co.id>
2022-10-13 18:38:30 +0500

    change end point

--- src/services/request.js
+++ src/services/request.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 // export var diaspora = "https://nimc-service.herokuapp.com";
 // export var baseUrl = "http://localhost:5000/api/";
-export var baseUrl = "https://facial-airtel.herokuapp.com/api/";
+export var baseUrl = "https://facial-recog-middleware.herokuapp.com/api/";
 export var postData = (route, data) => {
   return new Promise((resolve, reject) => {
     Axios.post(baseUrl + route, data)
